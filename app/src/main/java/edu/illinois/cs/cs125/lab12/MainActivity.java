c44409131346a1e1de32d5e2d13e3c4b89a99902
SARAH <sbm3@illinois.edu>
2018-04-29 21:45:36 -0500

    4.29.18 Got the button+search+textview to work

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -1,10 +1,15 @@
 package edu.illinois.cs.cs125.lab12;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,6 +21,9 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Main class for our UI design lab.
  */
@@ -25,6 +33,8 @@ public final class MainActivity extends AppCompatActivity {
 
     /** Request queue for our API requests. */
     private static RequestQueue requestQueue;
+    public TextView restaurantInfo;
+    public String queryInfo;
 
     /**
      * Run when this activity comes to the foreground.
@@ -37,14 +47,19 @@ public final class MainActivity extends AppCompatActivity {
 
         // Set up the queue for our API requests
         requestQueue = Volley.newRequestQueue(this);
-
         setContentView(R.layout.activity_main);
+
+        restaurantInfo = findViewById(R.id.textView);
+        restaurantInfo.setText("hello world");
+
+        final SearchView searchView = findViewById(R.id.searchText);
 
         // Attach the handler to our UI button
         final Button startAPICall = findViewById(R.id.search_restaurant);
         startAPICall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
+                queryInfo = searchView.getQuery().toString().replaceAll(" ", "+");
                 Log.d(TAG, "Start API button clicked");
                 startAPICall();
             }
@@ -66,16 +81,15 @@ public final class MainActivity extends AppCompatActivity {
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
-                    "https://api.yelp.com/v3/businesses/search"
-                            + BuildConfig.API_KEY,
+                    "https://api.yelp.com/v3/businesses/search?term=" + queryInfo + "&location=Champaign,61820",
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(final JSONObject response) {
                             try {
                                 Log.d(TAG, response.toString(2));
-
-
+                                String result = response.getJSONArray("businesses").getJSONObject(0).get("name").toString();
+                                restaurantInfo.setText(result);
                             } catch (JSONException ignored) { }
                         }
                     }, new Response.ErrorListener() {
@@ -83,7 +97,15 @@ public final class MainActivity extends AppCompatActivity {
                         public void onErrorResponse(final VolleyError error) {
                             Log.e(TAG, error.toString());
                         }
-                    });
+                    }) {
+                        @Override
+                        public Map<String, String> getHeaders() {
+                            Map<String, String>  params = new HashMap<String, String>();
+                            params.put("Authorization", "Bearer " + BuildConfig.API_KEY);
+                            Log.d(TAG, params.toString());
+                            return params;
+                        }
+            };
             requestQueue.add(jsonObjectRequest);
         } catch (Exception e) {
             e.printStackTrace();
