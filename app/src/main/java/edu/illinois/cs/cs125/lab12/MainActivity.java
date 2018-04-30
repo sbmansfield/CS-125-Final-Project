5c9ba9acda5b55ba89d8aebde1232c8774e69bce
SARAH <sbm3@illinois.edu>
2018-04-30 15:32:01 -0500

    4.30.18 app works for all 3 restaurants

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -63,7 +63,7 @@ public final class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         restaurantInfo = findViewById(R.id.textView);
-        restaurantInfo.setText("Click the search button to search for a nearby restaurant in Champaign!");
+        //restaurantInfo.setText("Click the search button to search for a nearby restaurant in Champaign!");
 
         final SearchView searchView = findViewById(R.id.searchText);
 
@@ -101,43 +101,84 @@ public final class MainActivity extends AppCompatActivity {
                         public void onResponse(final JSONObject response) {
                             try {
                                 Log.d(TAG, response.toString(2));
-                                JSONObject result = response.getJSONArray("businesses").getJSONObject(0);
-                                String name = result.get("name").toString();
 
-                                JSONArray categories = result.getJSONArray("categories");
-                                final String foodType = categories.getJSONObject(0).get("title").toString();
-
-                                final String rating = result.get("rating").toString();
-
-                                final String price = result.get("price").toString();
-
-                                final JSONArray addressArray = result.getJSONObject("location").getJSONArray("display_address");
-                                final String street = addressArray.get(0).toString();
-                                final String city = addressArray.get(1).toString();
-                                final String address = street + ", " + city;
-
-                                final String phoneNumber = result.get("display_phone").toString();
-
-                                final String url = result.get("image_url").toString();
+                                restaurantInfo.setText("Click the buttons below to display more information on each restaurant: ");
 
                                 //create the first restaurant info button
-                                Button restaurantName = findViewById(R.id.restaurant1);
-                                restaurantName.setVisibility(View.VISIBLE);
-                                restaurantName.setText(name);
-                                restaurantName.setOnClickListener(new View.OnClickListener() {
+                                JSONObject result = response.getJSONArray("businesses").getJSONObject(0);
+                                final String[] information = getRestaurantInfo(result);
+
+                                Button restaurant1 = findViewById(R.id.restaurant1);
+                                restaurant1.setVisibility(View.VISIBLE);
+                                restaurant1.setText(information[0]);
+                                restaurant1.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(final View v) {
                                         Log.d(TAG, "Start API button clicked");
                                         TextView info = findViewById(R.id.editText);
                                         info.setVisibility(View.VISIBLE);
-                                        String displayInfo = "Food Type: " + foodType + "\n"
-                                                + "Rating: " + rating + "\n"
-                                                + "Price: " + price + "\n"
-                                                + "Address: " + address + "\n"
-                                                + "Phone Number: " + phoneNumber + "\n";
+                                        String displayInfo = "Type: " + information[1] + "\n"
+                                                + "Rating: " + information[2] + "\n"
+                                                + "Price: " + information[3] + "\n"
+                                                + "Address: " + information[4] + "\n"
+                                                + "Phone Number: " + information[5];
 
                                         info.setText(displayInfo);
 
+                                        new DownloadImageTask((ImageView) findViewById(R.id.imageView))
+                                                .execute(information[6]);
+                                    }
+                                });
+
+                                //create the second restaurant info button
+                                JSONObject result2 = response.getJSONArray("businesses").getJSONObject(1);
+                                final String[] information2 = getRestaurantInfo(result2);
+
+                                Button restaurant2 = findViewById(R.id.restaurant2);
+                                restaurant2.setVisibility(View.VISIBLE);
+                                restaurant2.setText(information2[0]);
+                                restaurant2.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(final View v) {
+                                        Log.d(TAG, "Start API button clicked");
+                                        TextView info = findViewById(R.id.editText);
+                                        info.setVisibility(View.VISIBLE);
+                                        String displayInfo = "Type: " + information2[1] + "\n"
+                                                + "Rating: " + information2[2] + "\n"
+                                                + "Price: " + information2[3] + "\n"
+                                                + "Address: " + information2[4] + "\n"
+                                                + "Phone Number: " + information2[5];
+
+                                        info.setText(displayInfo);
+
+                                        new DownloadImageTask((ImageView) findViewById(R.id.imageView))
+                                                .execute(information2[6]);
+                                    }
+                                });
+
+                                //create the third restaurant info button
+                                JSONObject result3 = response.getJSONArray("businesses").getJSONObject(2);
+                                final String[] information3 = getRestaurantInfo(result3);
+
+                                Button restaurant3 = findViewById(R.id.restaurant3);
+                                restaurant3.setVisibility(View.VISIBLE);
+                                restaurant3.setText(information3[0]);
+                                restaurant3.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(final View v) {
+                                        Log.d(TAG, "Start API button clicked");
+                                        TextView info = findViewById(R.id.editText);
+                                        info.setVisibility(View.VISIBLE);
+                                        String displayInfo = "Type: " + information3[1] + "\n"
+                                                + "Rating: " + information3[2] + "\n"
+                                                + "Price: " + information3[3] + "\n"
+                                                + "Address: " + information3[4] + "\n"
+                                                + "Phone Number: " + information3[5];
+
+                                        info.setText(displayInfo);
+
+                                        new DownloadImageTask((ImageView) findViewById(R.id.imageView))
+                                                .execute(information3[6]);
                                     }
                                 });
 
@@ -160,6 +201,66 @@ public final class MainActivity extends AppCompatActivity {
             requestQueue.add(jsonObjectRequest);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    String[] getRestaurantInfo(JSONObject result) {
+        String[] informationArray = new String[7];
+        try {
+            Log.d(TAG, result.toString(2));
+            //JSONObject result = response.getJSONArray("businesses").getJSONObject(0);
+            String name = result.get("name").toString();
+            informationArray[0] = name;
+
+            JSONArray categories = result.getJSONArray("categories");
+            String foodType = categories.getJSONObject(0).get("title").toString();
+            informationArray[1] = foodType;
+
+            String rating = result.get("rating").toString();
+            informationArray[2] = rating;
+
+            String price = result.get("price").toString();
+            informationArray[3] = price;
+
+            JSONArray addressArray = result.getJSONObject("location").getJSONArray("display_address");
+            String street = addressArray.get(0).toString();
+            String city = addressArray.get(1).toString();
+            String address = street + ", " + city;
+            informationArray[4] = address;
+
+            String phoneNumber = result.get("display_phone").toString();
+            informationArray[5] = phoneNumber;
+
+            String url = result.get("image_url").toString();
+            informationArray[6] = url;
+
+        } catch (JSONException ignored) { }
+
+    return informationArray;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
         }
     }
 }
